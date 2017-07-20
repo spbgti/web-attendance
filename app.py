cce0f59310087611c0b3dd151ce74dc9563ede95
zakharovaomnom <zakharova.oliam@gmail.com>
2017-07-21 00:09:54 +0300

    ---

--- app.py
+++ app.py
@@ -2,12 +2,16 @@ from flask import Flask
 from datetime import datetime
 app = Flask(__name__)
 
+
 @app.route('/hello')
 def hello_world():
     return 'Hello World!'
 
+
 @app.route('/get_time')
 def get_now_datetime():
-    return datetime.now().isoformat()
+    s = datetime.strftime(datetime.today(), '%d.%m.%Y %H:%M %A')
+    return s
 
 app.run()
+#Я не понимаю где проблема в url, тк в теории должно работать, а на деле 404 и хоть убейся
