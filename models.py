3f78e9d98680a7ecb96180b7d89f26ad00e72cc4
zakharovaomnom <zakharova.oliam@gmail.com>
2017-08-03 20:34:46 +0300

    03/08/17

--- models.py
+++ models.py
@@ -1,14 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Table, Column, Integer, String
+from sqlalchemy.orm import mapper
+
+from database import metadata
 
 
 db = SQLAlchemy()
-
-
-class Student(db.Model):
-    id = db.Column(db.Integer, primary_key=True),
-    name = db.Column(db.String)
-    group_number = db.Column(db.String)
-
+# class Student(db.Model):
+class Student(object):
+    #    id = db.Column(db.Integer, primary_key=True),
+    #    name = db.Column(db.String)
+    #    group_number = db.Column(db.String)
     def __init__(self, name, group_number):
         self.name = name
         self.group_number = group_number
@@ -17,16 +19,33 @@ class Student(db.Model):
         return '<Student: %s, %s>' % (self.name, self.group_number)
 
 
-class Visit(db.Model):
-    id = db.Column(db.Integer, primary_key=True),
-    date = db.Column(db.String)
-    pair_num = db.Column(db.Integer)
-    student = db.Column(db.Integer)
+students = Table('students', metadata,
+                 Column('id', Integer, primary_key=True),
+                 Column('name', String),
+                 Column('group_number', String)
+                 )
+mapper(Student, students)
 
+
+# class Visit(db.Model):
+class Visit(object):
+    #    id = db.Column(db.Integer, primary_key=True),
+    #    date = db.Column(db.String)
+    #    pair_num = db.Column(db.Integer)
+    #    student = db.Column(db.Integer)
     def __init__(self, date, pair_num, student):
         self.date = date
         self.pair_num = pair_num
         self.student = student
 
     def __repr__(self):
-        return '<Visit: %s, %s, %s>' % (self.student_key, self.date, self.pair_num)
+        return '<Visit: %s, %s, %s>' % (self.student, self.date, self.pair_num)
+
+
+visits = Table('visits', metadata,
+               Column('id', Integer, primary_key=True),
+               Column('date', String),
+               Column('pair_num', Integer),
+               Column('student', Integer)
+               )
+mapper(Visit, visits)
