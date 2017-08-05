2d3eac1c607ec21acff8b4cd54ecf7524ff5e86c
zakharovaomnom <zakharova.oliam@gmail.com>
2017-08-05 17:08:58 +0300

    ii

--- main.py
+++ main.py
@@ -2,33 +2,29 @@ from models import db, Student, Visit
 from flask import Flask
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
 db.init_app(app)
 app.app_context().push()
-
+db.create_all()
 
 student = Student(name='Rick', group_number='161')
 db.session.add(student)
-db.session.commit()
-Rick = Visit(date='01.09.17', pair_num='2', student=student)
-db.session.add(Rick)
-db.session.commit()
-Rick = Visit(date='05.09.17', pair_num='4', student=student)
-db.session.add(Rick)
-db.session.commit()
-
 student = Student(name='Morty', group_number='162')
 db.session.add(student)
 db.session.commit()
+
+Rick = Visit(date='01.09.17', pair_num='2', student=student)
+db.session.add(Rick)
+Rick = Visit(date='05.09.17', pair_num='4', student=student)
+db.session.add(Rick)
+
 Morty = Visit(date='01.09.17', pair_num='2', student=student)
 db.session.add(Morty)
-db.session.commit()
 Morty = Visit(date='01.09.17', pair_num='3', student=student)
 db.session.add(Morty)
 db.session.commit()
 
-Rick = Student.query.filter(name='Rick').first()
-print(list(Visit.query.filter(student=Rick).all))  # Пары, на которых был Рик
-print(list(Visit.query.filter(date='01.09.17').all))  # Все посещения за 1 сентября
+Rick = Student.query.filter_by(name='Rick').first()
+print(list(Visit.query.filter_by(student=Rick).all))  # Пары, на которых был Рик
+print(list(Visit.query.filter_by(date='01.09.17').all))  # Все посещения за 1 сентября
