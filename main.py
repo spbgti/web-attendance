7f34798ca57c2505505914962d0be0af49a75af8
b0g3r <kotvberloge@gmail.com>
2017-08-05 20:48:14 +0300

    add get visits endpoint (see #3)

--- main.py
+++ main.py
@@ -1,34 +1,49 @@
-from datetime import date
+from datetime import date, datetime
 from models import db, Student, Visit
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
 db.init_app(app)
-app.app_context().push()
-db.create_all()
 
-student = Student(name='Rick', group_number='161')
-db.session.add(student)
-student = Student(name='Morty', group_number='162')
-db.session.add(student)
-db.session.commit()
 
-Rick = Student.query.filter_by(name='Rick').first()
-visit = Visit(date=date(2017, 9, 1), pair_num='2', student=Rick)
-db.session.add(visit)
-visit = Visit(date=date(2017, 9, 5), pair_num='4', student=Rick)
-db.session.add(visit)
+@app.route('/visits/student/<int:student_id>/date/<pair_date>', methods=['GET'])
+def get_visits(student_id, pair_date):
+    # вытаскиваем студента по id
+    student = Student.query.get(student_id)
 
-Morty = Student.query.filter_by(name='Morty').first()
-visit = Visit(date=date(2017, 9, 1), pair_num='2', student=Morty)
-db.session.add(visit)
-visit = Visit(date=date(2017, 9, 1), pair_num='3', student=Morty)
-db.session.add(visit)
-db.session.commit()
+    if student is None:  # если студент не найден - вернуть ошибку.
+        # Model.query.get возвращает None, если в базе нет записи с таким
+        # примари-кей.
+        # http://docs.sqlalchemy.org/en/latest/orm/query.html#sqlalchemy.orm.query.Query.get
+        return 'Student not found', 404
 
-Rick = Student.query.filter_by(name='Rick').first()
-print(list(Visit.query.filter_by(student=Rick).all()))  # Пары, на которых был Рик
-print(list(Visit.query.filter_by(date=date(2017, 9, 1)).all()))  # Все посещения за 1 сентября
+    try:  # если дату не получилось спарсить, то strptime выкинет исключение
+        # https://docs.python.org/3/library/datetime.html#datetime.datetime.strptime
+        pair_date = datetime.strptime(pair_date, '%Y.%m.%d').date()
+        # не забываем перевести datetime в date c помощью .date()
+    except ValueError:  # ловим исключение, и прописываем действие
+        return 'Invalid date format, try YYYY.MM.DD', 400
 
+    visits_by_day = Visit.query.filter_by(student=student, date=pair_date).all()
+
+    # заполняем все номера пар - False
+    pair_visits = {1: False, 2: False, 3: False, 4: False}
+
+    for visit in visits_by_day:  # перебираем все визиты студента за день
+        pair_visits[visit.pair_num] = True  # если он был на этой паре,
+        # то ставим True
+
+    # если visits_by_day - пустая последовательность (ни одного визита не
+    # было), то цикл не выполнится ни разу и все будут False (что логично)
+
+    # возвращаем json-результат как договарились в #3
+    return jsonify(visits={
+        'student': student_id,
+        'date': pair_date.strftime('%Y.%m.%d'),
+        'pairs': pair_visits
+    })
+
+if __name__ == '__main__':
+    app.run()
