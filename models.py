78aaf6318f4567b52eb2565db91c3b30c60aa754
zakharovaomnom <zakharova.oliam@gmail.com>
2017-08-07 19:52:17 +0300

    add UC

---
+++ models.py
@@ -0,0 +1,35 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import UniqueConstraint
+
+db = SQLAlchemy()
+
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    group_number = db.Column(db.String)
+    __student_args__ = (UniqueConstraint('name', 'group_number', name='Name_Group_UC'))
+
+    def __init__(self, name, group_number):
+        self.name = name
+        self.group_number = group_number
+
+    def __repr__(self):
+        return '<Student: %s, %s>' % (self.name, self.group_number)
+
+
+class Visit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date)
+    pair_num = db.Column(db.Integer)
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'))
+    student = db.relationship('Student')
+    __visit_args__ = (UniqueConstraint('date', 'pair_num', 'student', name='Stud_Date_Pair_UC'))
+
+    def __init__(self, date, pair_num, student):
+        self.date = date
+        self.pair_num = pair_num
+        self.student = student
+
+    def __repr__(self):
+        return '<Visit: %s, %s, %s>' % (self.student, self.date, self.pair_num)
