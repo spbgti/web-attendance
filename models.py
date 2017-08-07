58c0948dcd3a5402ad5885fb23dfbacd3bf4fd28
zakharovaomnom <zakharova.oliam@gmail.com>
2017-08-07 21:19:02 +0300

    testing base

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     group_number = db.Column(db.String)
-    __student_args__ = (UniqueConstraint('name', 'group_number', name='Name_Group_UC'))
+    __table_args__ = (db.UniqueConstraint('name', 'group_number', name='Name_Group_UC'),)
 
     def __init__(self, name, group_number):
         self.name = name
@@ -23,7 +23,7 @@ class Visit(db.Model):
     pair_num = db.Column(db.Integer)
     student_id = db.Column(db.Integer, db.ForeignKey('student.id'))
     student = db.relationship('Student')
-    __table_args__ = db.UniqueConstraint('date', 'pair_num', 'student', name='Stud_Date_Pair_UC')
+    __table_args__ = (db.UniqueConstraint('date', 'pair_num', 'student', name='Stud_Date_Pair_UC'),)
 
     def __init__(self, date, pair_num, student):
         self.date = date
