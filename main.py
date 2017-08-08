35b78aaeea79087791c9e2d45bb202798a25c4ea
zakharovaomnom <zakharova.oliam@gmail.com>
2017-08-08 17:44:37 +0300

    add post, delete for visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from datetime import date, datetime
 from models import db, Student, Visit
-from flask import Flask, jsonify
+from flask import Flask, jsonify, json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -44,6 +44,65 @@ def get_visits(student_id, pair_date):
         'date': pair_date.strftime('%Y.%m.%d'),
         'pairs': pair_visits
     })
+
+
+# исправить эндпоинт
+@app.route('/visits/student/<int:student_id>/date/<pair_date>/pair/<int:pair_num>/add', methods=['POST'])
+def post_visits(student_id, pair_date, pair_num):
+    # вытаскиваем студента по id
+    student = Student.query.get(student_id)
+
+    if student is None:  # если студент не найден - вернуть ошибку.
+        # Model.query.get возвращает None, если в базе нет записи с таким
+        # примари-кей.
+        # http://docs.sqlalchemy.org/en/latest/orm/query.html#sqlalchemy.orm.query.Query.get
+        return 'Student not found', 404
+
+    try:  # если дату не получилось спарсить, то strptime выкинет исключение
+        # https://docs.python.org/3/library/datetime.html#datetime.datetime.strptime
+        pair_date = datetime.strptime(pair_date, '%Y.%m.%d').date()
+        # не забываем перевести datetime в date c помощью .date()
+    except ValueError:  # ловим исключение, и прописываем действие
+        return 'Invalid date format, try YYYY.MM.DD', 400
+
+    #проверяем наличие данных о заданной паре
+    visit = Visit.query.filter_by(student=student, date=pair_date, pair_num=pair_num).all()
+
+    if visit is None:
+        visit = Visit(date=pair_date, pair_num=pair_num, student=student)
+        db.session.add(visit)
+        db.session.commit()
+        return 'Add', 200 # надо ли добавлять более подробные сведения
+    else:
+        return 'Data is in the DataBase', 200
+
+
+@app.route('/visits/student/<int:student_id>/date/<pair_date>/pair/<int:pair_num>', methods=['DELETE'])
+def delete_visits(student_id, pair_date, pair_num):
+    # вытаскиваем студента по id
+    student = Student.query.get(student_id)
+
+    if student is None:  # если студент не найден - вернуть ошибку.
+        # Model.query.get возвращает None, если в базе нет записи с таким
+        # примари-кей.
+        # http://docs.sqlalchemy.org/en/latest/orm/query.html#sqlalchemy.orm.query.Query.get
+        return 'Student not found', 404
+
+    try:  # если дату не получилось спарсить, то strptime выкинет исключение
+        # https://docs.python.org/3/library/datetime.html#datetime.datetime.strptime
+        pair_date = datetime.strptime(pair_date, '%Y.%m.%d').date()
+        # не забываем перевести datetime в date c помощью .date()
+    except ValueError:  # ловим исключение, и прописываем действие
+        return 'Invalid date format, try YYYY.MM.DD', 400
+
+    #проверяем наличие данных о заданной паре
+    visit = Visit.query.filter_by(student=student, date=pair_date, pair_num=pair_num).all()
+
+    if visit is None:
+        return 'Data not found', 200 # надо ли добавлять более подробные сведения
+    else:
+        visit = Visit.query.filter_by(student=student, date=pair_date, pair_num=pair_num).delete()
+        return 'Data deleted', 200
 
 if __name__ == '__main__':
     app.run()
